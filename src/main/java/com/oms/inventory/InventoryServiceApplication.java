565d0919d915369e547e43a567f217e09e44ee8b
RAJASEKAR VADAMALAIMUTHU <Rajasekar.Vadamalaimuthu@merrillcorp.com>
2017-10-05 12:15:04 +0530

    OMS-0013-Committing-Swagger-code

--- src/main/java/com/oms/inventory/InventoryServiceApplication.java
+++ src/main/java/com/oms/inventory/InventoryServiceApplication.java
@@ -1,11 +1,13 @@
 package com.oms.inventory;
 
+import com.oms.common.web.EnableOMSCommonWeb;
 import com.oms.inventory.repository.InventoryRepositoryCustom;
 import com.oms.inventory.repository.InventoryRepositoryCustomImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableOMSCommonWeb
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
